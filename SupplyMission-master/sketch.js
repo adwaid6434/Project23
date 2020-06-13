c4dade04373bb3d59986127a79fa30dae25dceba
adwaid6434 <adwaid6434@gmail.com>
2020-06-14 01:25:21 +0530

    project23

--- SupplyMission-master/sketch.js
+++ SupplyMission-master/sketch.js
@@ -1,5 +1,5 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground,box,boxsprite,box2,boxsprite2,box3,boxsprite3
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -25,19 +25,37 @@ function setup() {
 	helicopterSprite.scale=0.6
 
 	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	groundSprite.shapeColor=color(255);
 
+	boxsprite = createSprite(width/2,height-50,200,20);
+	boxsprite.shapeColor=color(255,0,0);
+
+	boxsprite2 = createSprite(width/2-110,height-90,20,100);
+	boxsprite2.shapeColor=color(255,0,0);
+	
+	boxsprite3 = createSprite(width/2+110,height-90,20,100);
+	boxsprite3.shapeColor=color(255,0,0);
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
+
+	box = Bodies.rectangle(width/2,height-65,200,20,{isStatic:true});
+	World.add(world,box);
+
+	box2 = Bodies.rectangle(width/2-110,height-90,20,100,{isStatic:true});
+	World.add(world,box);
+
+	box3 = Bodies.rectangle(width/2+110,height-90,20,100,{isStatic:true});
+	World.add(world,box);
+
 
 
 	Engine.run(engine);
